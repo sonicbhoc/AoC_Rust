6d586c75e4299b545d00ecdfed11c0fab48856c6
Charles Christie II <sonicbhoc@gmail.com>
2025-05-08 23:50:36 -0400

    Finalize Solver module; Fix AoC 2015 Day 1 to use new Solver

--- src/aoc_2015/day1.rs
+++ src/aoc_2015/day1.rs
@@ -1,86 +1,59 @@
-use crate::solver::{AoCDay, Solution, Puzzle};
+use crate::solver::{*};
+use std::fmt::Debug;
 
 struct Santa {
     current_floor: i64,
     steps_to_basement: u64,
 }
 
-enum Move {
+pub enum Move {
     Up,
     Down,
 }
 
-enum Errs {
+#[derive(Debug)]
+pub enum Errs {
     ParseError,
 }
 
-fn apply_move(val: &i64, dir: &Move) -> i64 {
+fn apply_move(floor: i64, dir: &Move) -> i64 {
     match dir {
-        Move::Up => val + 1,
-        Move::Down => val - 1,
+        Move::Up => floor + 1,
+        Move::Down => floor - 1,
     }
 }
 
-fn parse(s: &str) -> Result<&Vec<Move>, Errs> {
-    s.chars().collect().map(|c| match c {
+fn parse(s: &str) -> Result<Vec<Move>, Errs> {
+    s.chars().map(
+        |c| match c {
         '(' => Ok(Move::Up),
         ')' => Ok(Move::Down),
         _ => Err(Errs::ParseError),
-    })
+    }).collect()
 }
 
-pub fn aoc_day_1 (s: &str) -> AoCDay<&Vec<Move>, i64, u64, Errs> {
+pub fn aoc_day_1 (s: &str) -> AoCDay<Vec<Move>, i64, u64, Errs> {
     AoCDay {
         parser: parse,
-        puzzle_part1:
-    }
-}
-
-fn follow_directions(santa: Santa, directions: &Vec<Move>) -> Santa {
-    Santa {
-        current_floor: directions.iter().fold(0, apply_move),
-        steps_to_basement: santa.steps_to_basement,
-    }
-}
-
-fn locate_basement(santa: Santa, directions: &Vec<Move>) -> Santa {
-    Santa {
-        current_floor: santa.current_floor,
-        steps_to_basement: directions
-            .iter()
-            .scan(0i64, |acc, dir| match acc {
-                -1 => None,
-                acc => Some(apply_move(acc, dir)),
+        puzzle_part1: Puzzle {
+            solver: (|movements| Solution { problem: "Final Floor".to_string(), answer: Some(movements.iter().fold(0, apply_move)) })
+        },
+        puzzle_part2: Puzzle {
+            solver: (|movements| Solution {
+                problem: "Basement Position".to_string(),
+                answer: Some (
+                    movements
+                        .iter()
+                        .scan(
+                            0, 
+                            |acc, dir| match acc {
+                                -1 => None,
+                                acc => Some(apply_move(*acc, dir))
+                            })
+                        .count()
+                        as u64
+                )
             })
-            .count() as u64,
-    }
-}
-
-struct AoCDay1<'a, 'b, T, U> {
-    answers: AoCDay<'a, 'b, T, U>,
-}
-
-impl Solver<i64, u64, Errs> for AoCDay1<i64, u64> {
-    fn solve(&self, dataset: &str) -> Result<(), Errs> {
-        parse(dataset).map(|data| {
-            let santa = Santa {
-                current_floor: 0,
-                steps_to_basement: 0,
-            };
-
-            let santa = follow_directions(santa, &data);
-            let santa = locate_basement(santa, &data);
-
-            self.answers = AoCDay {
-                answer_part_1: Solution {
-                    problem: "Final Floor: ",
-                    answer: &santa.current_floor,
-                },
-                answer_part_2: Some(Solution {
-                    problem: "Basement Step: ",
-                    answer: &santa.steps_to_basement,
-                }),
-            }
-        })
+        },
     }
 }
--- src/solver.rs
+++ src/solver.rs
@@ -1,37 +1,42 @@
-use std::fmt::Display;
+use std::fmt::{Debug, Display};
 
-pub struct Solution<'a, T: Display> {
-    pub problem: &'a str,
-    pub answer: &'a T
+pub struct Solution<T: Display> {
+    pub problem: String,
+    pub answer: Option<T>,
 }
 
-impl Display for Solution<_> {
-    fn fmt(self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "{}: {}", self.problem, self.answer)
+impl<'a, T: Display> Display for Solution<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match &self.answer {
+            Some(ans) => write!(f, "{}: {}", self.problem, ans),
+            None => write!(f, "{}", self.problem),
+        }
     }
 }
 
-pub struct Puzzle<TProblemInput, TProblemOutput> {
-    pub solver: fn(&TProblemInput) -> Solution<TProblemOutput>
+pub struct Puzzle<TProblemInput, TProblemOutput: Display> {
+    pub solver: fn(&TProblemInput) -> Solution<TProblemOutput>,
 }
 
-pub struct AoCDay<'a, 'b, TPuzzleIn, TPuzzle1Out, TPuzzle2Out, TPuzzleErr> {
+pub struct AoCDay<TPuzzleIn, TPuzzle1Out: Display, TPuzzle2Out: Display, TPuzzleErr: Debug> {
     pub parser: fn(&str) -> Result<TPuzzleIn, TPuzzleErr>,
-    pub puzzle_part1: Puzzle<&'a TPuzzleIn, TPuzzle1Out>,
-    pub puzzle_part2: Option<Puzzle<&'b TPuzzleIn, TPuzzle2Out>>
+    pub puzzle_part1: Puzzle<TPuzzleIn, TPuzzle1Out>,
+    pub puzzle_part2: Puzzle<TPuzzleIn, TPuzzle2Out>,
 }
 
-impl AoCDay<_, _, _, _> {
+impl<TPuzzleIn, TPuzzle1Out: Display, TPuzzle2Out: Display, TPuzzleErr: Debug>
+    AoCDay<TPuzzleIn, TPuzzle1Out, TPuzzle2Out, TPuzzleErr>
+{
     fn solve(self, puzzle_input: &str) {
         let parse = self.parser;
-        
+
         parse(puzzle_input)
             .map(|input| {
-                let result = String::new();
-                let day1 = self.puzzle_part1.solver(&input);
-                let maybe_day2 = self.puzzle_part2.map(|pt2| pt2.solver(&input));
-                
-                write!(result, "{}\n{}", day1, maybe_day2.unwrap_or("")).unwrap();
+                let day1 = (self.puzzle_part1.solver)(&input);
+                let day2 = (self.puzzle_part2.solver)(&input);
+
+                println!("Solutions:\n{day1}\n{day2}")
             })
+            .unwrap()
     }
 }
